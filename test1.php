6ce3cd80ae87b9e6838fe59fcfcf2cb9681be2b2
David-Rosas <davidrosas1987@gmail.com>
2023-08-07 07:22:00 -0500

    pack apple for my friend in the file test2.js

--- test1.php
+++ test1.php
@@ -1,8 +1,18 @@
 <?php
 
 function is_palindrome($input) {
-    //solve this method that says if the sentence is palindrome or not
+    // Convert the input string to lowercase and remove non-alphanumeric characters
+    $input = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $input));
+
+    // Reverse the input string
+    $reversed = strrev($input);
+
+    // Compare the original input with the reversed input
+    return $input === $reversed;
 }
 
 $input = "Allí por la tropa portado, traído a ese paraje de maniobras, una tipa como capitán usar boina me dejara, pese a odiar toda tropa por tal ropilla";
+
+// call function is_palindrome
 var_dump(is_palindrome($input));
+
--- test2.js
+++ test2.js
@@ -1,7 +1,56 @@
-var input = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+function organizeApples(
+  apples,
+  applesPerBox = 4,
+  boxesPerPack = 2,
+  packsPerFriend = 2
+) {
+  // Calculate the total number of apples, boxes, packs, and friends needed
+  const totalApples = apples.length;
+  const totalBoxes = Math.ceil(totalApples / applesPerBox);
+  const totalPacks = Math.ceil(totalBoxes / boxesPerPack);
+  const totalFriends = Math.ceil(totalPacks / packsPerFriend);
 
-function apples(input) {
+  // Initialize the output array to store the organized apples
+  const output = [];
+  let boxCounter = 0;
+  let exit_loops = false;
+  // Loop through each friend and create their packs of boxes
+  for (let friend = 0; friend < totalFriends; friend++) {
+    const friendPacks = [];
+    // Loop through each pack and create their boxes
+    for (let pack = 0; pack < packsPerFriend; pack++) {
+    if (exit_loops) break;
+      const packBoxes = [];
+      // Loop through each box and add apples to them
+      for (let box = 0; box < boxesPerPack; box++) {
+        // Slice the array to get the apples for the current box
+        const boxApples = apples.slice(
+          boxCounter * applesPerBox,
+          (boxCounter + 1) * applesPerBox
+        );
+        if (boxApples.length != 0) {
+          boxCounter++;
+          packBoxes.push(boxApples);
+        } else {
+          exit_loops = true;
+          break;
+        }
+      }
+      
+      // Add the pack of boxes to the friend's packs
+      if(packBoxes.length > 0)
+      friendPacks.push(packBoxes);
+    }
+    // Add the friend's packs to the output
+    output.push(friendPacks);
+  }
 
+  // Return the organized apples
+  return output;
 }
 
-console.log(apples(input))
+const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
+const output = organizeApples(input);
+
+// Output the result
+console.log(output);
